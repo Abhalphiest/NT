5dfa121129d9d35b3d848bed3168ae6f24444bcb
Margaret <med7068@rit.edu>
2016-04-23 13:51:49 -0400

    committing some half finished stuff so I can transfer machines. FermatSimple is almost implemented

--- Implementation/Primality/FermatTest/FermatSimple/FermatSimple.c
+++ Implementation/Primality/FermatTest/FermatSimple/FermatSimple.c
@@ -20,9 +20,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//upper bound for a
+const unsigned long UPP_BOUND = 1000000000;
+
 //function declarations
 void usageExit(char* msg);
-long fermatTest(long n);
+unsigned long fermatTest(unsigned long n);
+unsigned long powMod(unsigned long a, unsigned long pow, unsigned long m);
+unsigned long gcd(unsigned long a, unsigned long b);
 
 /*
  * main function validates input before passing it along to the test
@@ -35,10 +40,19 @@ int main(int argc, char** argv)
 {
  if(argc != 2)
   usageExit("You have an incorrect number of arguments.");
- long n = strtol(argv[1],NULL, 10);
+ unsigned long n =(unsigned long) strtol(argv[1],NULL, 10);
  if(n <= 0)
   usageExit("Your n is not a valid positive integer.");
- return 0;
+ 
+ unsigned long result = fermatTest(n);
+ if(result == 0)
+ {
+   fprintf(stdout, "%ld may be a prime number. \n", n);
+   return 0;
+ }
+
+ fprintf(stdout, "%ld proves %ld to be composite. \n",result, n);
+ return result;
 }
 
 /*
@@ -54,14 +68,17 @@ int main(int argc, char** argv)
  *
  */
 
-long fermatTest(long n)
+unsigned long fermatTest(unsigned long n)
 {
- long a = 2;	//1 should not be tested
- long p = n-1;  //save ourselves repeated subtraction
- while(a*a < n)	//less costly than sqrt computation
+ unsigned long a = 2;	//1 should not be tested
+ unsigned long p = n-1;  //save ourselves repeated subtraction
+ while(a < UPP_BOUND)	//less costly than sqrt computation
  {
-  if(powMod(a,p,n) == 1)
+  fprintf(stderr, "%lu %lu \n", a,powMod(a,p,n));
+  if(gcd(a,n) == 1 && powMod(a,p,n) != 1)
    return a;
+  a++; //will try to come up with a more sophisticated way 
+       //to choose reasonable a in the future
  }
  
  //if we get here, there were no witnesses found
@@ -79,23 +96,27 @@ long fermatTest(long n)
  *
  */
 
-long powMod(long a, long pow, long m)
+unsigned long powMod(unsigned long a,unsigned long pow,unsigned long m)
 {
- long prod = 1;
- long a_pow = a*a; //our current 
- if(pow%2 == 1) //odd exponent, shouldn't come up much in 
- {		//our current application but for the sake of code flexibility
-   prod *= a;
-   prod--;	//pull the non power of two exponent out
+ unsigned long prod = 1;
+ unsigned long a_pow = a; //our current
+ while(pow >0)
+ { 
+  if(pow%2 == 1) //odd exponent, shouldn't come up much in 
+  {		//our current application but for the sake of code flexibility
+    prod *= a_pow % m;
+    pow--;	//pull the non power of two exponent out
+		//technically this is a reduntant line with how integer division
+		//works, but I think it makes it more clear and readable
+    
+  }
+  //we now have an even exponent, which can be written as a sum of powers
+  //of two with exponents greater than 1.
+   pow /= 2; //divide by two
+   a_pow = a_pow*a_pow; //square
+   
  }
- //we now have an even exponent, which can be written as a sum of powers
- //of two with exponents greater than 1.
- while(pow > 0)
- {
-  p /= 2; //divide by two
-  if(pow%2 ==1)
- }
-
+ return prod % m;
 }
 
 /*
