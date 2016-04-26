57e0f219d28ec8837844733e723031bb4b2e49d3
Margaret <med7068@rit.edu>
2016-04-26 12:28:40 -0400

    FermatBig is implemented, compiled, tested exactly once. Needs more testing, but thats not a today thing

--- Implementation/Primality/FermatTest/FermatBig/FermatBig.c
+++ Implementation/Primality/FermatTest/FermatBig/FermatBig.c
@@ -14,13 +14,11 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<gmp.h>
 
 //function declarations
 void usageExit(char* msg);
-unsigned long fermatTest(unsigned long n, unsigned long b);
-unsigned long powMod(unsigned long a, unsigned long pow, unsigned long m);
-unsigned long gcd(unsigned long a, unsigned long b);
+void fermatTest(mpz_t n, mpz_t b, mpz_t result);
 
 /*
  * main function validates input before passing it along to the test
@@ -33,23 +31,44 @@ int main(int argc, char** argv)
 {
  if(argc != 3)
   usageExit("You have an incorrect number of arguments.");
- unsigned long n =(unsigned long) strtol(argv[1],NULL, 10);
- if(n <= 0)
-  usageExit("Your n is not a valid positive integer.");
-
- unsigned long b = (unsigned long) strtol(argv[2], NULL, 10);
- if(b <= 0)
-  usageExit("Your b is not a valid positive integer.");
  
- unsigned long result = fermatTest(n, b);
- if(result == 0)
+ //set up our really big integers
+ //these need to be freed with mpz_clear() before exiting the program
+ mpz_t n;
+ mpz_init(n);
+ mpz_t b;
+ mpz_init(b);
+
+ int ierr = mpz_set_str(n, argv[1], 10);
+ if(ierr < 0)
  {
-   fprintf(stdout, "%ld may be a prime number. \n", n);
+  mpz_clear(n);
+  mpz_clear(b);
+  usageExit("Your n is not a valid positive integer.");
+ }
+ ierr  = mpz_set_str(b, argv[2], 10);
+ if(ierr < 0)
+ {
+  mpz_clear(n);
+  mpz_clear(b);
+  usageExit("Your b is not a valid positive integer.");
+ }
+ 
+ mpz_t result;
+ mpz_init(result);
+ fermatTest(n, b, result);
+ if(mpz_cmp_ui(result, (unsigned long int) 0) == 0)
+ {
+   fprintf(stdout, "%s may be a prime number. \n", mpz_get_str(NULL, 10,n));
    return 0;
  }
 
- fprintf(stdout, "%ld proves %ld to be composite. \n",result, n);
- return result;
+ fprintf(stdout, "%s proves %s to be composite. \n",mpz_get_str(NULL, 10,result)
+						   ,mpz_get_str(NULL, 10, n));
+ mpz_clear(n);
+ mpz_clear(b);
+ mpz_clear(result);
+ return 0;
 }
 
 /*
@@ -57,29 +76,54 @@ int main(int argc, char** argv)
  * theorem to determine whether n can be proven composite.
  *
  * params:	n - the positive integer to be tested for compositeness
- *
- * return:	either the first witness to compositeness found or 0 if
+ *		b - the upper bound to check against n
+ * 		result - to pass back to the caller
+ *		either the first witness to compositeness found or 0 if
  *		not proven composite.
  *
  * Author:	Margaret Dorsey
  *
  */
 
-unsigned long fermatTest(unsigned long n, unsigned long b)
+void fermatTest(mpz_t n, mpz_t b, mpz_t result)
 {
- unsigned long a = 2;	//1 should not be tested
- unsigned long p = n-1;  //save ourselves repeated subtraction
- while(a < b)	//havent hit the upper bound yet
+ mpz_t a;
+ mpz_init(a);
+ mpz_set_ui(a, (unsigned long int) 2);	//1 should not be tested
+
+ mpz_t p;
+ mpz_init(p);
+ mpz_sub_ui(p,n,(unsigned long int) 1);  //save ourselves repeated subtraction
+
+ mpz_t gcd;
+ mpz_init(gcd);
+ mpz_t powmod;
+ mpz_init(powmod);
+ while(mpz_cmp(a,b) < 0)	//havent hit the upper bound yet
  {
-  // fprintf(stderr, "%lu %lu \n", a,powMod(a,p,n));
-  if(gcd(a,n) == 1 && powMod(a,p,n) != 1)
-   return a;
-  a++; //will try to come up with a more sophisticated way 
-       //to choose reasonable a in the future
+  mpz_gcd(gcd,a,n); //get the gcd of a and n and store in gcd
+  mpz_powm(powmod,a,p,n);
+  if(mpz_cmp_ui(gcd, (unsigned long int) 1) == 0
+     && mpz_cmp_ui(powmod, (unsigned long int) 1 != 0))
+  {
+    mpz_clear(gcd);
+    mpz_clear(powmod);
+    mpz_clear(p);
+    mpz_set(result,a);
+    mpz_clear(a);
+    return;
+  }
+  mpz_add_ui(a, a, (unsigned long int) 1);
  }
  
  //if we get here, there were no witnesses found
- return 0;
+ mpz_clear(gcd);
+ mpz_clear(powmod);
+ mpz_clear(a);
+ mpz_clear(p); //it would be faster to clear these as a null terminated list
+		//but this is more illustrative
+ mpz_set_ui(result, (unsigned long int) 0);
+ return;
 }
 
 /*
@@ -115,38 +159,6 @@ unsigned long powMod(unsigned long a,unsigned long pow,unsigned long m)
    
  }
  return prod % m;
-}
-
-/*
- * computes the gcd of two numbers recursively
- *
- * params:	a - first integer
- *			b - second integer
- *
- * returns:	gcd(a,b)
- *
- * Author:	Margaret Dorsey
- */
- 
-unsigned long gcd(unsigned long a, unsigned long b)
-{
- unsigned long r;
- if(a > b)
- {
-   r = a%b;
-   if(r == 0)//base case
-    return b;
-   return gcd(r,b);
- } 
- else
- {
-   r=b%a;
-   if(r == 0)
-    return a;
-   return gcd(a,r);
-   
- }
-
 }
 
 /*
